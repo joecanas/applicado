a80175babc004eb9392f1b6d1d9b16d6264060e5
Joe Canas <joecanas1@gmail.com>
2019-01-27 13:51:45 -0800

    interim push to set admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,51 +35,49 @@ User.create([{
 }])
 
 # Employers
-employer1 = User.create({
+employer1 = User.create(
   :first_name => "Employer1",
   :last_name => "Lastname1",
   :email => "employer1@test.com",
   :password => "password",
   :password_confirmation => "password",
   :role => 1
-})
+)
 
-employer2 = User.create({
+employer2 = User.create(
   :first_name => "Employer2",
   :last_name => "Lastname2",
   :email => "employer2@test.com",
   :password => "password",
   :password_confirmation => "password",
   :role => 1
-})
+)
 
-employer3 = User.create({
-  :first_name => "Employer1",
+employer3 = User.create(
+  :first_name => "Employer3",
   :last_name => "Lastname3",
   :email => "employer3@test.com",
   :password => "password",
   :password_confirmation => "password",
   :role => 1
-})
+)
 
 # Administrator
-User.create({
+User.create(
   :first_name => "Admin1",
   :last_name => "Lastname1",
   :email => "admin1@test.com",
   :password => "password",
   :password_confirmation => "password",
   :role => 2
-})
+)
 
 # Create sample job openings for each of the employers
 
 # Jobs - Employer1
 Job.create(
   title: "Customer Service Manager",
-  description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-
-    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+  description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
   company: "Bamboolean",
   location: "San Francisco",
   company_id: employer1.id
@@ -87,11 +85,7 @@ Job.create(
 
 Job.create(
   title: "Senior Software Engineer",
-  description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
-
-    All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. 
-
-    The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+  description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.  The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
   company: "Audio League",
   location: "San Francisco",
   company_id: employer1.id
@@ -124,9 +118,7 @@ Job.create(
 
 Job.create(
   title: "IT/DevOps Analyst",
-  description: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-
-    Nulla tempor sem orci, non dapibus nisi feugiat at. Duis faucibus est vitae molestie tristique. Sed a pharetra nisi. Curabitur accumsan lectus non venenatis scelerisque. Maecenas ac laoreet felis. Donec ut semper quam. Proin maximus justo ex, vestibulum viverra est convallis at. Proin euismod nulla nibh, suscipit venenatis neque sodales non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ut ipsum auctor, tincidunt nisl a, venenatis ante. Donec sed elit pharetra, placerat lacus sed, finibus augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam semper ut arcu quis placerat.",
+  description: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla tempor sem orci, non dapibus nisi feugiat at. Duis faucibus est vitae molestie tristique. Sed a pharetra nisi. Curabitur accumsan lectus non venenatis scelerisque. Maecenas ac laoreet felis. Donec ut semper quam. Proin maximus justo ex, vestibulum viverra est convallis at. Proin euismod nulla nibh, suscipit venenatis neque sodales non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ut ipsum auctor, tincidunt nisl a, venenatis ante. Donec sed elit pharetra, placerat lacus sed, finibus augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam semper ut arcu quis placerat.",
   company: "Feenominal",
   location: "Mountain View",
   company_id: employer2.id
@@ -134,9 +126,7 @@ Job.create(
 
 Job.create(
   title: "Chief of Staff - IT",
-  description: "Sed et elit nulla. Donec id semper diam. Quisque congue sapien vel blandit ultricies. Morbi sed porta nunc. Fusce elementum scelerisque ante, a venenatis odio malesuada vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut blandit sem nec ipsum porttitor molestie. In hac habitasse platea dictumst. Etiam orci massa, lacinia ut dictum eget, lobortis eget leo. Nullam quis massa semper nisl gravida pellentesque eget at enim. In vulputate blandit mauris, eget tincidunt mi ultricies eleifend. Aliquam ac eleifend magna, at tincidunt nulla. Donec ante tellus, condimentum eu magna quis, bibendum pharetra orci. Nam quis est pulvinar, vestibulum elit ac, sollicitudin mi. Curabitur ut massa sit amet sapien varius malesuada. Donec sed mattis sapien, nec congue nisl.
-
-    Duis nec ex leo. Etiam vitae aliquam libero, ut tincidunt lacus. Sed vitae aliquam erat, vitae interdum ipsum. Donec ornare erat nec dolor viverra, condimentum feugiat leo commodo. Sed consectetur turpis risus, nec tempor sapien tincidunt vitae. Morbi eleifend eros euismod justo luctus posuere. Maecenas vel fringilla eros, nec volutpat neque. Donec vel nulla laoreet, varius erat et, accumsan libero. Phasellus orci dolor, rhoncus sed mauris ac, sagittis eleifend orci. Sed finibus venenatis mi, sed vestibulum erat porttitor at.",
+  description: "Sed et elit nulla. Donec id semper diam. Quisque congue sapien vel blandit ultricies. Morbi sed porta nunc. Fusce elementum scelerisque ante, a venenatis odio malesuada vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut blandit sem nec ipsum porttitor molestie. In hac habitasse platea dictumst. Etiam orci massa, lacinia ut dictum eget, lobortis eget leo. Nullam quis massa semper nisl gravida pellentesque eget at enim. In vulputate blandit mauris, eget tincidunt mi ultricies eleifend. Aliquam ac eleifend magna, at tincidunt nulla. Donec ante tellus, condimentum eu magna quis, bibendum pharetra orci. Nam quis est pulvinar, vestibulum elit ac, sollicitudin mi. Curabitur ut massa sit amet sapien varius malesuada. Donec sed mattis sapien, nec congue nisl. Duis nec ex leo. Etiam vitae aliquam libero, ut tincidunt lacus. Sed vitae aliquam erat, vitae interdum ipsum. Donec ornare erat nec dolor viverra, condimentum feugiat leo commodo. Sed consectetur turpis risus, nec tempor sapien tincidunt vitae. Morbi eleifend eros euismod justo luctus posuere. Maecenas vel fringilla eros, nec volutpat neque. Donec vel nulla laoreet, varius erat et, accumsan libero. Phasellus orci dolor, rhoncus sed mauris ac, sagittis eleifend orci. Sed finibus venenatis mi, sed vestibulum erat porttitor at.",
   company: "Feenominal",
   location: "San Mateo",
   company_id: employer2.id
@@ -144,9 +134,7 @@ Job.create(
 
 Job.create(
   title: "Project Manager",
-  description: "Duis non nisi blandit nibh venenatis porta id sit amet eros. Etiam in eleifend augue, et suscipit nibh.
-
-    Phasellus dictum sodales blandit. Sed ac vestibulum erat, eu volutpat magna. Vestibulum nec venenatis lorem, sed malesuada neque. Phasellus finibus fermentum porta. Sed eget nulla nec elit malesuada sollicitudin.",
+  description: "Duis non nisi blandit nibh venenatis porta id sit amet eros. Etiam in eleifend augue, et suscipit nibh. Phasellus dictum sodales blandit. Sed ac vestibulum erat, eu volutpat magna. Vestibulum nec venenatis lorem, sed malesuada neque. Phasellus finibus fermentum porta. Sed eget nulla nec elit malesuada sollicitudin.",
   company: "Feenominal",
   location: "San Francisco",
   company_id: employer2.id
@@ -179,9 +167,7 @@ Job.create(
 
 Job.create(
   title: "Head of Commercial Software",
-  description: "In id interdum odio. Ut non erat enim. Pellentesque a odio non ipsum fringilla maximus. Integer sit amet maximus dui, in consectetur lacus. Cras ac tincidunt elit, vitae laoreet erat. Duis vel dolor quis eros accumsan malesuada. Phasellus non volutpat nunc, in semper erat. Donec placerat sem egestas ex tristique iaculis. Aenean feugiat sit amet felis vitae tincidunt.
-
-    Maecenas in lectus felis. Suspendisse eleifend ipsum eget neque suscipit vehicula. Nulla sed lobortis nunc. Mauris at ligula a ex faucibus dictum. Pellentesque in placerat lorem. Fusce tristique venenatis nisi, in ornare dolor finibus non. Duis in tincidunt neque. Vestibulum ac faucibus dolor. Mauris ac eleifend sapien, at venenatis dolor. Aliquam euismod dignissim neque, blandit pellentesque dui tempor nec.",
+  description: "In id interdum odio. Ut non erat enim. Pellentesque a odio non ipsum fringilla maximus. Integer sit amet maximus dui, in consectetur lacus. Cras ac tincidunt elit, vitae laoreet erat. Duis vel dolor quis eros accumsan malesuada. Phasellus non volutpat nunc, in semper erat. Donec placerat sem egestas ex tristique iaculis. Aenean feugiat sit amet felis vitae tincidunt. Maecenas in lectus felis. Suspendisse eleifend ipsum eget neque suscipit vehicula. Nulla sed lobortis nunc. Mauris at ligula a ex faucibus dictum. Pellentesque in placerat lorem. Fusce tristique venenatis nisi, in ornare dolor finibus non. Duis in tincidunt neque. Vestibulum ac faucibus dolor. Mauris ac eleifend sapien, at venenatis dolor. Aliquam euismod dignissim neque, blandit pellentesque dui tempor nec.",
   company: "Casheteria",
   location: "San Francisco",
   company_id: employer3.id
